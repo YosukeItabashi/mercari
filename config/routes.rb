d523ae3a9ddebdea305a72e9495cc64cf646fb23
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-06-02 11:57:21 +0900

    Createアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'items#index'
   devise_for :users
   get   'users/:id'   =>  'users#show'
-  resources :items, only: [:show, :new]
+  resources :items, only: [:create, :show, :new]
 end
