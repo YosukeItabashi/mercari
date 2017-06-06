b425a11489448816b7c3ef21d8a9b296e16cddbb
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-06-06 21:41:28 +0900

    モデルのアソシエーション形成とバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  belongs_to :saler, class_name: "User"
+  belongs_to :buyer, class_name: "User"
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :state, presence: true
+  validates :postage, presence: true
+  validates :region, presence: true
+  validates :shipping_date, presence: true
+  validates :price, presence: true
+
+  def sold?
+    buyer.present?
+  end
 end
