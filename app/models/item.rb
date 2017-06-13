1e7df71d48b3deebba9547ed27a4eaa3cf24a0cf
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-06-13 20:21:24 +0900

    微修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,6 @@ class Item < ApplicationRecord
   belongs_to :buyer, class_name: "User"
 
   mount_uploader :image, ImageUploader
-
   validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
