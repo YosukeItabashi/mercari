c232025f9c65a1c7bdfb07d666cc39e7376ca9a9
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-06-13 12:45:55 +0900

    商品一覧で「全体公開」のアイテムのみ表示されるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :purchase, :edit, :destroy, :update]
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.where(release: "publick").order("created_at DESC")
   end
 
   def show
@@ -15,7 +15,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(name: item_params[:name], image: item_params[:image], description: item_params[:description], category: item_params[:category], state: item_params[:state], postage: item_params[:postage], region: item_params[:region], shipping_date: item_params[:shipping_date], price: item_params[:price], saler_id: current_user.id)
+    @item = Item.new(name: item_params[:name], image: item_params[:image], description: item_params[:description], category: item_params[:category], state: item_params[:state], postage: item_params[:postage], region: item_params[:region], shipping_date: item_params[:shipping_date], price: item_params[:price], release: item_params[:release], saler_id: current_user.id)
     if @item.save
       redirect_to "/items", notice: "商品を投稿しました"
     else
@@ -50,11 +50,11 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :image, :description, :category, :state, :postage, :region, :shipping_date, :price)
+    params.require(:item).permit(:name, :image, :description, :category, :state, :postage, :region, :shipping_date, :price, :release)
   end
 
   def set_item
-    @item = Item.find(params[:id])
+    @item = Item.friendly.find(params[:id])
   end
 
 end
