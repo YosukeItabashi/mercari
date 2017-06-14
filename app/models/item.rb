02329638aa7234f2c3a3592a1c42d66dada8dccc
YosukeItabashi <yosuke.itabashi@itahashiyousukenoMacBook.local>
2017-06-14 19:46:10 +0900

    Commentsテーブルとのアソシエーション形成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User"
+  has_many :comments
 
   mount_uploader :image, ImageUploader
   validates :image, presence: true
